80e926a20b61960b6ce35da24f3b6576d3bfa65a
Enzo <Enzopetryort@gmail.com>
2022-05-09 15:31:15 -0300

    `Teste`

---
+++ src/main.java
@@ -0,0 +1,20 @@
+public class main {
+    private int Num1;
+    private String Nome;
+
+    public int getNum1() {
+        return Num1;
+    }
+
+    public void setNum1(int num1) {
+        Num1 = num1;
+    }
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+}
